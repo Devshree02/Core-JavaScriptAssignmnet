34f1f3d4cbb1a948bf14c635a9b119f6c8fd00b0
Devshree02 <devshreenighojakar0205@gmail.com>
2021-09-11 21:21:50 +0530

    Completed Code with JQuery

--- assests/javascript/custom.js
+++ assests/javascript/custom.js
@@ -36,27 +36,37 @@
 //   resetForm();
   
 // });
+
 $(document).ready(function(){
 
   $("#addNewButton").click(function(){
     $("<input>").attr({
-      placeholder : "Enter your name" ,
-      class : "nameClass"
+      placeholder : "Enter your name",
+      class : "nameClass" 
     }).appendTo("#mainId");
 
   }); 
 
+  function resetForm() {
+
+
+    $(".nameClass").each(function() {
+      $(this).remove();
+    })
+    $("#name").val("");
+  }
+
   $("#submitButton").click(function(){
 
-    let ele = $(".nameClass").length;
-    // let len =ele.length;
-    window.alert(ele);
+     // window.alert("submitButton");
+     $(".mainClass input").each(function(index) {
+      value = $(this).val();
+      markup = "<tr><td>"+index+1+"</td><td>"+value+"</td></tr>";
+      tableBody = $("table");
+      tableBody.append(markup);
+    });
+     resetForm();
 
-    value = $( "#name" ).val();
-    markup = "<tr><td>"+value+"</td></tr>";
-    tableBody = $("table");
-    tableBody.append(markup);
-    
-  }); 
+   }); 
 
 }); 
